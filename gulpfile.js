d629b5d0d8584f078efa99ec1947be5cc7df9d6d
Dan Holloran <dtholloran@gmail.com>
2015-07-09 19:23:46 -0500

    Adds gulp file

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+// Set Directores
+var distSrc        = './dist';
+var assetsSrc      = './_assets/';
+var jsSrc          = assetsSrc + '/js/**/*.js';
+var jsDistSrc      = distSrc + '/js/';
+var scssSrc        = assetsSrc + '/scss/**/*.scss';
+var cssDistSrc     = distSrc + '/css';
+var mapsSrc        = './maps/';
+var mapsDistSrc    = distSrc + '/maps/';
+var imageUploadSrc = '/_uploads/**/*.[png,gif,jpg,jpeg]';
+var imageAssetSrc  = assetsSrc + '/img/**/*.[png,gif,jpg,jpeg]';
