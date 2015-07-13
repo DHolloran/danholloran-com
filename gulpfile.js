560220da17a97408bff9292a193bbab785eb4452
Dan Holloran <dtholloran@gmail.com>
2015-07-12 19:59:13 -0500

    Adds Gulp LiveReload

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,7 @@
+// Require all the things!!!!
+var gulp       = require( 'gulp' );
+var livereload = require( 'gulp-livereload' );
+
 // Set Directores
 var distSrc        = './dist';
 var assetsSrc      = './_assets/';
@@ -9,3 +13,12 @@ var mapsSrc        = './maps/';
 var mapsDistSrc    = distSrc + '/maps/';
 var imageUploadSrc = '/_uploads/**/*.[png,gif,jpg,jpeg]';
 var imageAssetSrc  = assetsSrc + '/img/**/*.[png,gif,jpg,jpeg]';
+
+gulp.task( 'watch', function() {
+  livereload.listen();
+  gulp.watch( '_site/**/*' );
+} );
+
+gulp.task( 'default', [ 'watch' ], function() {
+
+} );
