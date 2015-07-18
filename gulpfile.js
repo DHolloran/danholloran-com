99aa9b4e6189ff6aecaaa3ce70a93b224366c7de
Dan Holloran <dtholloran@gmail.com>
2015-07-17 21:20:11 -0500

    Sets up styles Gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,59 @@
 // Require all the things!!!!
-var gulp       = require( 'gulp' );
-var livereload = require( 'gulp-livereload' );
+var gulp          = require( 'gulp' );
+var livereload    = require( 'gulp-livereload' );
+var sass          = require( 'gulp-sass' );
+var sourcemaps    = require( 'gulp-sourcemaps' );
+var postcss       = require( 'gulp-postcss' );
+var changed       = require( 'gulp-changed' );
+var autoprefixer  = require( 'autoprefixer-core' );
+var cssnano       = require( 'gulp-cssnano' );
+var mqpacker      = require( 'css-mqpacker' );
+var singleCharset = require( 'postcss-single-charset' );
+var inlineImport  = require( 'postcss-import' );
+var notify        = require( 'gulp-notify' );
+var scsslint = require( 'gulp-scss-lint' );
 
-// Set Directores
+// Set Directories.
 var distSrc        = './dist';
 var assetsSrc      = './_assets/';
 var jsSrc          = assetsSrc + '/js/**/*.js';
 var jsDistSrc      = distSrc + '/js/';
 var scssSrc        = assetsSrc + '/scss/**/*.scss';
 var cssDistSrc     = distSrc + '/css';
-var mapsSrc        = './maps/';
+var mapsSrc        = '../maps/';
 var mapsDistSrc    = distSrc + '/maps/';
 var imageUploadSrc = '/_uploads/**/*.[png,gif,jpg,jpeg]';
 var imageAssetSrc  = assetsSrc + '/img/**/*.[png,gif,jpg,jpeg]';
 
+// Styles Task
+gulp.task( 'styles', function() {
+  // PostCSS Processors.
+  var processors = [
+        autoprefixer(),
+        mqpacker,
+        singleCharset,
+        inlineImport,
+      ]
+  ;
+
+  gulp.src( scssSrc )
+    .pipe( changed( cssDistSrc ) )
+    .pipe( sourcemaps.init() )
+    .pipe( scsslint() )
+    .pipe( scsslint.failReporter( 'E' ) )
+    .pipe( sass().on( 'error', sass.logError ) )
+    .pipe( postcss( processors ) )
+    .pipe( cssnano() )
+    .pipe( sourcemaps.write( mapsSrc ) )
+    .pipe( gulp.dest( cssDistSrc ) )
+    .pipe( notify( 'Styles task complete!' ) );
+} );
+
+// Watch Task.
 gulp.task( 'watch', function() {
   livereload.listen();
-  gulp.watch( '_site/**/*' );
+  gulp.watch( scssSrc, [ 'styles' ] );
 } );
 
-gulp.task( 'default', [ 'watch' ], function() {
-
-} );
+// Default Task.
+gulp.task( 'default', [ 'watch' ], function() {} );
