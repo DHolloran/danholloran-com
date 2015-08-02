47f88933f50268e0d90081c4b1e63736d016e794
Dan Holloran <dtholloran@gmail.com>
2015-08-02 09:53:56 -0500

    Removes Jekyll/Broswersync

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ var browserSync = require( 'browser-sync' );
 var cp          = require( 'child_process' );
 var request     = require( 'request' );
 var $           = require( 'gulp-load-plugins' )( {
-  pattern : [ 'gulp-*', 'gulp.*', 'postcss-*' ]
+	pattern : [ 'gulp-*', 'gulp.*', 'postcss-*' ]
 } );
 
 // Set Directories.
@@ -25,65 +25,37 @@ var htmlDistSrc        = '_site/**/*.html';
 var siteURL            = 'http://danholloran.me';
 var sitemapURL         = siteURL + '/sitemap.xml';
 
-// Jekyll build drafts
-gulp.task( 'jekyll:build:drafts', function( done ) {
-    browserSync.notify( 'Building Jeykll (--drafts)' );
-    return cp.spawn( 'bundle', [ 'exec', 'jekyll', 'build', '--drafts' ], { stdio: 'inherit' } )
-        .on( 'close', done );
-} );
-
-// Jekyll rebuild drafts
-gulp.task( 'jekyll:rebuild:drafts', [ 'jekyll:build:drafts' ], function() {
-    browserSync.reload();
-} );
-
-// Browser Sync
-gulp.task( 'browser-sync', [ 'build' ], function() {
-    browserSync( {
-        server : {
-            baseDir : '_site'
-        }
-    } );
-} );
-
-/**
- * Rebuild Jekyll & do page reload
- */
-gulp.task( 'jekyll:rebuild', [ 'jekyll:build:drafts' ], function() {
-    browserSync.reload();
-} );
-
 // Clean task.
 gulp.task( 'clean', function( cb ) {
-  del( [
-    distSrc + '/**/*',
-  ], cb );
+	del( [
+		distSrc + '/**/*',
+	], cb );
 } );
 
 // Image uploads task.
 gulp.task( 'images:uploads', function() {
-  gulp.src( imageUploadSrc )
-    .pipe( $.changed( imageUploadDistSrc ) )
-    .pipe( $.imagemin( {
-        optimizationLevel : 3,
-        progressive       : true,
-        interlaced        : true
-    } ) )
-    .pipe( gulp.dest( imageUploadDistSrc ) )
-    .pipe( $.notify( 'Image uploads task complete!' ) );
+	gulp.src( imageUploadSrc )
+		.pipe( $.changed( imageUploadDistSrc ) )
+		.pipe( $.imagemin( {
+				optimizationLevel : 3,
+				progressive       : true,
+				interlaced        : true
+		} ) )
+		.pipe( gulp.dest( imageUploadDistSrc ) )
+		.pipe( $.notify( 'Image uploads task complete!' ) );
 } );
 
 // Image assets task.
 gulp.task( 'images:assets', function() {
-  return gulp.src( imageAssetSrc )
-    .pipe( $.changed( imageAssetDistSrc ) )
-    .pipe( $.imagemin( {
-        optimizationLevel : 3,
-        progressive       : true,
-        interlaced        : true
-    } ) )
-    .pipe( gulp.dest( imageAssetDistSrc ) )
-    .pipe( $.notify( 'Images assets task complete!' ) );
+	return gulp.src( imageAssetSrc )
+		.pipe( $.changed( imageAssetDistSrc ) )
+		.pipe( $.imagemin( {
+				optimizationLevel : 3,
+				progressive       : true,
+				interlaced        : true
+		} ) )
+		.pipe( gulp.dest( imageAssetDistSrc ) )
+		.pipe( $.notify( 'Images assets task complete!' ) );
 } );
 
 // Images task
@@ -91,56 +63,44 @@ gulp.task( 'images', [ 'images:uploads', 'images:assets' ] );
 
 // Styles Task
 gulp.task( 'styles', function() {
-  // PostCSS Processors.
-  var processors = [
-        require( 'autoprefixer-core' )( 'last 1 version' ),
-        require( 'css-mqpacker' )(),
-        $.postcssSingleCharset,
-        $.postcssImport,
-      ]
-  ;
+	// PostCSS Processors.
+	var processors = [
+				require( 'autoprefixer-core' )( 'last 1 version' ),
+				require( 'css-mqpacker' )(),
+				$.postcssSingleCharset,
+				$.postcssImport,
+			]
+	;
 
-  gulp.src( scssSrc )
-    .pipe( $.changed( cssDistSrc ) )
-    .pipe( $.sourcemaps.init() )
-    .pipe( $.scssLint() )
-    .pipe( $.scssLint.failReporter( 'E' ) )
-    .pipe( $.sass().on( 'error', $.sass.logError ) )
-    .pipe( $.postcss( processors ) )
-    .pipe( $.cssnano() )
-    .pipe( $.sourcemaps.write( mapsSrc ) )
-    .pipe( gulp.dest( cssDistSrc ) )
-    .pipe( $.notify( 'Styles task complete!' ) );
+	gulp.src( scssSrc )
+		.pipe( $.changed( cssDistSrc ) )
+		.pipe( $.sourcemaps.init() )
+		.pipe( $.scssLint() )
+		.pipe( $.scssLint.failReporter( 'E' ) )
+		.pipe( $.sass().on( 'error', $.sass.logError ) )
+		.pipe( $.postcss( processors ) )
+		.pipe( $.cssnano() )
+		.pipe( $.sourcemaps.write( mapsSrc ) )
+		.pipe( gulp.dest( cssDistSrc ) )
+		.pipe( $.notify( 'Styles task complete!' ) );
 } );
 
 // Build task.
-gulp.task( 'build', [ 'images', 'styles', 'jekyll:build:drafts' ] );
-
-// Clean build
-gulp.task( 'clean:build', [ 'clean' ], function() {
-  gulp.start( 'build' );
-} );
-
-// Deploy build.
-gulp.task( 'deploy:build', [ 'clean:build', 'rake' ] );
+gulp.task( 'build', [ 'images', 'styles' ] );
 
 // Gulp seo task.
 gulp.task( 'seo', [ 'build' ], function( cb ) {
-    request( 'http://www.google.com/webmasters/tools/ping?sitemap=' + sitemapURL );
-    request( 'http://www.bing.com/webmaster/ping.aspx?siteMap=' + sitemapURL );
-    cb();
+		request( 'http://www.google.com/webmasters/tools/ping?sitemap=' + sitemapURL );
+		request( 'http://www.bing.com/webmaster/ping.aspx?siteMap=' + sitemapURL );
+		cb();
 } );
-
-// Rake task.
-gulp.task( 'rake', $.shell.task( [ 'rake' ] ) );
 
 // Watch Task.
 gulp.task( 'watch', [ 'build' ], function() {
-  gulp.watch( scssSrc, [ 'styles' ] );
-  gulp.watch( [ 'index.html', '_layouts/*.html', '_posts/*', '_drafts/*' ], [ 'jekyll:rebuild' ] );
-  gulp.watch( [ imageUploadSrc ], [ 'images:uploads' ] );
-  gulp.watch( [ imageAssetSrc ], [ 'images:asset' ] );
+	gulp.watch( scssSrc, [ 'styles' ] );
+	gulp.watch( [ imageUploadSrc ], [ 'images:uploads' ] );
+	gulp.watch( [ imageAssetSrc ], [ 'images:asset' ] );
 } );
 
 // Default Task.
-gulp.task( 'default', [ 'browser-sync', 'watch' ] );
+gulp.task( 'default', [ 'watch' ] );
